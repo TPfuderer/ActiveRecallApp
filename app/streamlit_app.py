51e1a97bbf3f1aae62a56cfd91e7b9e918e690b9
Tristan Pfuderer <pfudi200@gmail.com>
2025-11-27 10:34:09 +0100

    Rename

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -121,7 +121,6 @@ with tabs[0]:
         theme="dracula",
         key=f"ace_editor_{task['id']}",
         height=200,
-        submit_on_enter=True,  # <-- NEU
     )
 
     # =====================================================================
